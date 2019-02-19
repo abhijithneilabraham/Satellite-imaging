e1d612f719e657492ed4f5cdc9e40549b58e1143
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-02-20 00:11:57 +0530

    good

---
+++ buildingmaps.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 20 00:07:36 2019
+
+@author: abhijithneilabraham
+"""
+
+
+import cv2
+import numpy as np
+img = cv2.imread("test.tiff")
+frame = img[100:900, 100:900]
+frame1 = cv2.resize(frame,(1000,1000))
+lower_range = np.array([0,0,0])
+upper_range =np.array([255,115,255])
+mask = cv2.inRange(frame1, lower_range, upper_range)
+cv2.imshow("Map",mask)
+cv2.imwrite("Test.jpg",mask)
